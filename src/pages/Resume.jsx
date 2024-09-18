bda06559e8948d499a743d4a02f8c8b55f56d1f0
Sauryadeep123 <sauryadeep2023@gmail.com>
2024-09-18 21:12:43 +0530

    change

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,15 +4,16 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
 import Particle from '../components/Particle'
-import pdf from "../assets/sajib.pdf"
+import pdf from "../assets/Resume/ResumeLink.pdf"
 import LetsConnect from '../components/LetsConnect';
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import ResumeLink from "../assets/Resume/ResumeLink.pdf"
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/sajib.pdf`
+// const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/sajib.pdf`
 
 
 const Resume = () => {
@@ -20,7 +21,7 @@ const Resume = () => {
 
   useEffect(() => {
     
-    setWidth(window.innerWidth);
+    setWidth(700);
   }, []);
 
   return (
@@ -40,8 +41,8 @@ const Resume = () => {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document file={ResumeLink} className="d-flex justify-content-center pdf-preview">
+            <Page  pageNumber={1} scale={width > 786 ? 1.7 : 0.6}  />
           </Document>
         </Row>
 
